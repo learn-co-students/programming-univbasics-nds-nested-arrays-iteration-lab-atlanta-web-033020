58c94ba430a2ab110cbe24265f0915b2878d46c7
Christina Placide <christina.placide@hotmail.com>
2020-03-19 19:09:41 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,44 @@
+require "pry"
+
+#array_1 = [['pepperoni', 'sausage'], ['green olives', 'green peppers'], ['onions', 'pineapple']]
+
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+stringy = []
+array_1 = 0
+  while array_1 < src.count do
+    ingredients = src[array_1]
+    stringy.push("I love #{ingredients[0]} and #{ingredients[1]} on my pizza")
+ array_1 +=1
+  end 
+  stringy
 end
 
+#i did this no help omg!!
+#array_2 = [[-1, -900], [10, 30], [0, 0], [14, 16 * -2.5], [Math.sin(1), 19]]
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
-end
+largest = []
+array_2 = 0
+    while array_2 < src.count do 
+      pairs = src[array_2]
+      largest.push(pairs.max())
+      array_2 += 1
+    end
+    largest
+end  
+
+#array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+index = 0
+binding.pry
+sum = 0
+ while index < src.length do 
+   if src[index][0] %2 == 0 && src[index][1] %2 == 0
+  sum += ( src[index][0] + src[index][1])
+  end 
+  index += 1
 end
+sum
+end
+
+#while src[0][0] && src[0][1] %2 == 0 do
